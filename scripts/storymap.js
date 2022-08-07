2f01e49b0fbcf4fa664b53973a42571a6ed7f5dc
codefordelhi <random.walker1@gmail.com>
2022-08-06 20:13:21 -0400

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -485,9 +485,9 @@ $(window).on('load', function() {
       credit += ' | ';
     }
 
-    credit += 
+    credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
     if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
-    credit +=;
+    credit += ' with ';
     $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
   }
 
